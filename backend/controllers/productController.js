c12a3101145243a576a74a99b8581904a2884a4f
binhnguyen96html <binhnguyenbiz96@gmail.com>
2024-01-04 01:44:57 -0800

    edit pagination

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -6,7 +6,7 @@ import Product from "../models/productModel.js";
 // @access Public
 const getProducts = asyncHandler(async (req, res) => {
   //Pagination
-  const pageSize = PAGINATION_LIMIT;
+  const pageSize = process.env.PAGINATION_LIMIT;
   const page = Number(req.query.pageNumber) || 1;
 
   //Search
